7bfcf2a93928782655bfad578ab5e901aaa8f880
chcbz <chcbz@sina.com>
2020-05-11 12:13:14 +0800

    优化礼品表

---
+++ jia-mapper-point/src/main/java/cn/jia/point/entity/GiftExample.java
@@ -0,0 +1,54 @@
+package cn.jia.point.entity;
+
+public class GiftExample extends Gift {
+
+    private Integer clientStrictFlag;
+
+    private Long createTimeStart;
+
+    private Long createTimeEnd;
+
+    private Long updateTimeStart;
+
+    private Long updateTimeEnd;
+
+    public Integer getClientStrictFlag() {
+        return clientStrictFlag;
+    }
+
+    public void setClientStrictFlag(Integer clientStrictFlag) {
+        this.clientStrictFlag = clientStrictFlag;
+    }
+
+    public Long getCreateTimeStart() {
+        return createTimeStart;
+    }
+
+    public void setCreateTimeStart(Long createTimeStart) {
+        this.createTimeStart = createTimeStart;
+    }
+
+    public Long getCreateTimeEnd() {
+        return createTimeEnd;
+    }
+
+    public void setCreateTimeEnd(Long createTimeEnd) {
+        this.createTimeEnd = createTimeEnd;
+    }
+
+    public Long getUpdateTimeStart() {
+        return updateTimeStart;
+    }
+
+    public void setUpdateTimeStart(Long updateTimeStart) {
+        this.updateTimeStart = updateTimeStart;
+    }
+
+    public Long getUpdateTimeEnd() {
+        return updateTimeEnd;
+    }
+
+    public void setUpdateTimeEnd(Long updateTimeEnd) {
+        this.updateTimeEnd = updateTimeEnd;
+    }
+}
