7321accfb0d6558f1842ff1585755953c57ae67f
chcbz <chcbz@sina.com>
2021-02-11 17:30:54 +0800

    优化代码规范

--- jia-core/src/main/java/cn/jia/core/util/thread/ThreadRequestContent.java
+++ jia-core/src/main/java/cn/jia/core/util/thread/ThreadRequestContent.java
@@ -11,13 +11,24 @@ public abstract class ThreadRequestContent {
 		this.attr = attr;
 	}
 
-	public void onSuccess() { // 执行成功的动作。用户可以覆盖此方法
-		System.out.println("onSuccess");
+	/**
+	 * 执行成功的动作。用户可以覆盖此方法
+	 */
+	public void onSuccess() {
+
 	}
 
-	public void onFailure() { // 执行失败的动作。用户可以覆盖此方法
-		System.out.println("onFailure");
+	/**
+	 * 执行失败的动作。用户可以覆盖此方法
+	 */
+	public void onFailure() {
+
 	}
 
-	public abstract void doSomeThing() throws Exception; // 用户必须实现这个抽象方法，告诉子线程要做什么
+	/**
+	 * 用户必须实现这个抽象方法，告诉子线程要做什么
+	 *
+	 * @throws Exception 异常
+	 */
+	public abstract void doSomeThing() throws Exception;
 }
