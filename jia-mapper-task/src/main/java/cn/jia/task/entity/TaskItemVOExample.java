dd078f9db8b2de6c5196db7aab391e9f6fb21012
chcbz <chcbz@sina.com>
2021-08-21 15:15:21 +0800

    修复TaskItemVO字段缺失问题

--- jia-mapper-task/src/main/java/cn/jia/task/entity/TaskItemVOExample.java
+++ jia-mapper-task/src/main/java/cn/jia/task/entity/TaskItemVOExample.java
@@ -1,35 +1,10 @@
 package cn.jia.task.entity;
 
 import lombok.Data;
-
-import java.math.BigDecimal;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class TaskItemVOExample {
-	private Integer id;
-
-    private Integer planId;
-
-    private String clientId;
-
-    private String jiacn;
-
-    private Integer type;
-
-    private Integer period;
-
-    private String crond;
-
-    private String name;
-
-    private String description;
-
-    private BigDecimal amount;
-
-    private Integer remind;
-
-    private Integer status;
-
+public class TaskItemVOExample extends TaskItemVO {
     private Long timeStart;
     
     private Long timeEnd;
