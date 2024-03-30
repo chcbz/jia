902b828eceaea072c58941acc04113a974f71454
chcbz <chcbz@sina.com>
2024-03-30 11:23:29 +0800

    新增jasypt加密组件，进行配置文件内容加密。

---
+++ common/jia-common-starter/src/test/java/cn/jia/core/JasyptTest.java
@@ -0,0 +1,40 @@
+package cn.jia.core;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.junit.jupiter.api.Test;
+
+public class JasyptTest {
+    @Test
+    public void encrypt() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("123456");
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        System.out.println(encryptor.encrypt("jia_dev"));
+    }
+
+    @Test
+    public void decrypt() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("123456");
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        System.out.println(encryptor.decrypt(
+                "eZ+ev8ac6+SZHL4Y1zSEIAKHChJONtmiE9furv4dSYxf0XL/0V4jW5RhuvpuaJ7o"));
+    }
+}
---
+++ common/jia-common-test/src/main/java/cn/jia/test/RabbitMqServerLoader.java
@@ -0,0 +1,32 @@
+package cn.jia.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * 内置RabbitMq服务器加载
+ *
+ * @author chc
+ */
+@Slf4j
+@Component
+@Testcontainers
+public class RabbitMqServerLoader {
+    @Value("${spring.rabbitmq.port:5672}")
+    private int port;
+    @Value("${spring.rabbitmq.username:test}")
+    private String username;
+    @Value("${spring.rabbitmq.password:test}")
+    private String password;
+
+    @Container
+    public final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3.8")
+            .withExposedPorts(port)
+            .withEnv("RABBITMQ_DEFAULT_USER", username)
+            .withEnv("RABBITMQ_DEFAULT_PASS", password);
+
+}
